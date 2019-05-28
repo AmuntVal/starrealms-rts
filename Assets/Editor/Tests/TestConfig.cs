9f9e869a9720100254a1addbdfcf2259afb66168
Lucas James <lucas.m.james92@gmail.com>
2019-05-28 17:42:56 +0000

    Added unit testing framework

---
+++ Assets/Editor/Tests/TestConfig.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestConfig {
+
+    [Test]
+    public void TestConfigSimplePasses() {
+        // Use the Assert class to test conditions.
+        Assert.Pass();
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator TestConfigWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+        Assert.Pass();
+    }
+}
